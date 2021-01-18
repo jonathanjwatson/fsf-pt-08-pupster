d8fca449559dc8ccbf6eb72f433eaa3092ecdc5e
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-01-18 18:52:00 -0500

    converted to hash router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import About from "./containers/About/About";
 import Discover from "./containers/Discover/Discover";
 import Search from "./containers/Search/Search";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/discover" component={Discover} />
